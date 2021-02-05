9f607ded2a79dceb8cb2ced06e50529296cc0058
lars pijnappel <larspijnappel@users.noreply.github.com>
2021-02-05 13:35:37 +0100

    Create get_tabyl_2.R
    add function `get_tabyl_2.R()` which includes promised arguments being handled correctly.

---
+++ r_functions/get_tabyl_2.R
@@ -0,0 +1,22 @@
+get_tabyl_2 <- function(df = df, by_row = by_row, by_col = by_col, sort_by = Total) {
+  library(janitor)
+  
+  ## embrace promised arguments
+  ## see vignette("programming") section Indirection
+  
+  df %>%
+    tabyl({{ by_row }}, {{by_col}}) %>% 
+    # adorn_totals(where = c("row", "col")) %>%
+    adorn_totals("col") %>%
+    adorn_percentages() %>% 
+    adorn_pct_formatting() %>% 
+    adorn_ns() %>%arrange(desc({{ sort_by }})) %>%
+    as_tibble()
+}
+
+# storms %>% get_tabyl_2(name, status)
+# storms %>% get_tabyl_2(by_row = category, by_col = status, sort_by = category)
+# storms %>% get_tabyl_2(category, status)
+# mtcars %>% get_tabyl_2(cyl, am, cyl)
+# mtcars %>% get_tabyl_2(carb, cyl, carb)
+# mtcars %>% get_tabyl_2(gear, cyl)
